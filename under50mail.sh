909bc7f5a201f1e7e0f91e7c11b2159cbe0f24cf
Adalen Vladi <adalenvladi@gmail.com>
2018-05-22 10:39:32 +0200

    Update under50mail.sh

--- under50mail.sh
+++ under50mail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d`
-url="http://192.168.1.206/vicidial/AST_agent_time_detail.php?DB=&query_date=$DATE&end_date=$DATE&group%5B%5D=--ALL--&user_group%5B%5D=--ALL--&shift=ALL&report_display_type=TEXT&SUBMIT=SUBMIT&mail_report=1&email=adalenvladi@gmail.com"
+url="http://192.168.1.206/vicidial/AST_agent_time_detail.php?DB=&query_date=$DATE&end_date=$DATE&group%5B%5D=--ALL--&user_group%5B%5D=--ALL--&shift=ALL&report_display_type=TEXT&SUBMIT=SUBMIT&cdr_pass=password&asterisk_pass=password&mail_report=1&email=adalenvladi@gmail.com"
 wget  --http-user=username --http-password=@password   -O- -q  $url > /dev/null
